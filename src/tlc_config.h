86e0ca880d845db9c86a327f1395592d53dc1821
danmo24 <91718685+danmo24@users.noreply.github.com>
2025-01-19 20:53:36 +0100

    Update tlc_config.h

--- src/tlc_config.h
+++ src/tlc_config.h
@@ -1,27 +1,25 @@
-
 #ifndef TLC_CONFIG_H
 #define TLC_CONFIG_H
 
+// PINS for XIAO_ESP32C3 see https://wiki.seeedstudio.com/XIAO_ESP32C3_Getting_Started/
+#define HSPI_MISO  D9  // TLC: Do not connect
+#define HSPI_MOSI  D10 // TLC: SIN
+#define HSPI_SCLK  D8  // TLC: SCLK
 
-#define HSPI_MISO 12 // BLANK
-#define HSPI_MOSI 13 // SIN
-#define HSPI_SCLK 14 // SCLK 
-#define HSPI_CS   15 // XLAT 
+#define XLAT_PIN  D4   // TLC: XLAT
 
-#define GSCLK_PIN 27
+#define GSCLK_PIN D5   // TLC: GSCLK
 #define GSCLK_FREQ 10e6
 #define GSCLK_TIMER 0
 #define GSCLK_TIMER_RESULUTION 1
 
-#define BLANK_PIN 26
+#define BLANK_PIN D2   // TLC: BLANK
 #define BLANK_FREQ 2441
 #define BLANK_HIGH 1
 #define BLANK_RESULUTION 15
 
-
-
 #ifndef NUM_TLCS
-#define NUM_TLCS  3
+#define NUM_TLCS  1
 #endif
 
 #endif
